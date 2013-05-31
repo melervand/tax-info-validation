cd73407c88c236fbc0b48eedd285857c3517e6d0
melervand <melervand@gmail.com>
2013-05-31 06:06:01 +0400

    Revert "add error handling"
    This reverts commit dc6bb3b58d042326ee1e13824e93488a6d2b27ad.

--- src/tax.php
+++ src/tax.php
@@ -4,7 +4,7 @@ class Tax {
         $tin = trim($tin);
         //TIN = Taxpayer identification number
         //ИНН = Идентификационный номер налогоплательщика
-        //http://ru.wikipedia.org/wiki/%D0%98%D0%9D%D0%9D#.D0.92.D1.8B.D1.87.D0.B8.D1.81.D0.BB.D0.B5.D0.BD.D0.B8.D0.B5_.D0.BA.D0.BE.D0.BD.D1.82.D1.80.D0.BE.D0.BB.D1.8C.D0.BD.D1.8B.D1.85_.D1.86.D0.B8.D1.84.D1.80.5B.D0.B8.D1.81.D1.82.D0.BE.D1.87.D0.BD.D0.B8.D0.BA.C2.A0.D0.BD.D0.B5.C2.A0.D1.83.D0.BA.D0.B0.D0.B7.D0.B0.D0.BD.C2.A0140.C2.A0.D0.B4.D0.BD.D0.B5.D0.B9.5D
+		//http://ru.wikipedia.org/wiki/%D0%98%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80_%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%89%D0%B8%D0%BA%D0%B0
         if (strlen($tin) == 12 || strlen($tin) == 10) {
             if (strlen($tin) == 12) {
                 //TIN is 12 digit number
@@ -68,9 +68,7 @@ class Tax {
         //BIC - Bank Identification Code/Банковский идентификационный код
 
         $ca = trim($ca);
-        if (strlen($ca) < 20) return false;
         $bic = trim($bic);
-        if (strlen($bic) < 9) return false;
         return self::validateBAN('0'.substr($bic, 4, 2).$ca);
     }
 
@@ -79,11 +77,7 @@ class Tax {
         //BIC - Bank Identification Code/Банковский идентификационный код
 
         $cura = trim($cura);
-        if (strlen($cura) < 20) return false;
         $bic = trim($bic);
-        if (strlen($bic) < 9) return false;
         return self::validateBAN(substr($bic, 6,3).$cura);
     }
-
-    //TODO: add http://ru.wikipedia.org/wiki/Код_причины_постановки_на_учёт
 }
